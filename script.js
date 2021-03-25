567dac31125d5aaa6f153a8b2df4df6545c3549e
Jorge Condori <condori.jrge@gmail.com>
2021-03-25 02:23:32 -0300

    Agregado la interactividad

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ let playerTurn = true;
 let turn = document.getElementById("turno");
 let cell = document.getElementsByTagName("td");
 
-cell.addEventListener(event => {
-    if(playerTurn){
-        console.log(event.target)
-    }
-})
+for(let td of cell){
+    td.addEventListener('click', event => {
+        if(playerTurn){
+            if(event.target.textContent == ""){
+                event.target.textContent = "X";
+                playerTurn = false;
+            }
+            else{
+                alert("Casillero ocupado");
+            }
+        }
+        else{
+            if(event.target.textContent == ""){
+                event.target.textContent = "O";
+                playerTurn = true;
+            }
+            else{
+                alert("Casillero ocupado");
+            }
+        }
+    })
+}
 
